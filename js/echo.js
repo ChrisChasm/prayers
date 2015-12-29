53dfae8f14bfbdcc156e090e3cea038c04958372
Kaleb Heitzman <kalebheitzman@gmail.com>
2015-12-29 11:13:21 -0500

    saving clicks to local storage

--- js/echo.js
+++ js/echo.js
@@ -11,10 +11,30 @@
 			// post the form
 			$.post('#', formData, function(data) {
 				// update the prayer count
-				var count = parseInt( $('span.echo-prayer-count.prayer-' + prayer_id).text(), 10 );
-				
+				var count = parseInt( $('span.echo-prayer-count.prayer-' + prayer_id).text(), 10 );				
+				// update visual display
 				$('span.echo-prayer-count.prayer-' + prayer_id).text(count+1);
+				
+				// store this click in local storage to prevent abuse
+				var items = localStorage.getItem('echo');
 
+				// localStorage echo-prayers key doesn't exist
+				if (items == null) {
+					items = { prayers: [ prayer_id ] };
+				}
+				else {
+					items = localStorage.getItem('echo');
+					items = JSON.parse(items);
+
+					if (items.prayers.indexOf(prayer_id) < 0) {						
+						items.prayers.push(prayer_id);
+					}
+				}
+				data = JSON.stringify(items);
+				localStorage.setItem( 'echo', data );
+
+				//console.log(localStorage);				
+				//localStorage.clear();
 			}, 'html');
 
 			event.preventDefault();
