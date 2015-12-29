db2eadc01919e345f976fc1d52eb5a22c60aca7e
Kaleb Heitzman <kalebheitzman@gmail.com>
2015-12-29 15:26:27 -0500

    added admin styles

--- includes/post_type_columns.php
+++ includes/post_type_columns.php
@@ -11,12 +11,12 @@ function echo_prayers_columns_head( $columns ) {
 	$category = $columns['taxonomy-prayer_category'];
 	unset($columns['taxonomy-prayer_category']);
 
-	$columns['prayer_submitter'] = __( 'Submitter', 'echo' );
 	$columns['prayer_location'] = __( 'Location', 'echo' );
-	$columns['prayer_coordinates'] = __( 'Coordinates', 'echo' );
-	$columns['prayer_count'] = __( 'Prayed', 'echo' );
-
 	$columns['taxonomy-prayer_category'] = $category;
+	$columns['prayer_count'] = __( 'Prayed', 'echo' );
+	$columns['prayer_answered'] = __( 'Answered?', 'echo' );
+	$columns['prayer_anonymous'] = __( 'Anonymous?', 'echo' );
+
 	$columns['date'] = $date;
 
 	return $columns;
@@ -33,36 +33,50 @@ function echo_prayers_columns( $column_name, $post_ID ) {
 		echo $prayer_count;
 	}
 	if ($column_name == 'prayer_location' ) {
-		$prayer_location = $post_meta['meta-prayer-location'][0];
-		$prayer_country = $post_meta['meta-prayer-location-country-long'][0];
-		echo $prayer_location . "<br />" . $prayer_country;
-
-		if ( ! empty($lat) && ! empty($long) ) {
-			echo '<div class="row-actions">';
-			echo '<a href="http://maps.google.com/?ie=UTF8&hq=&ll=' . $lat . ',' . $long . '&z=13" target="_blank">Map</a>';
-			echo '</div>';			
-		}
-	}
-	if ($column_name == 'prayer_submitter' ) {
+		
 		$prayer_name = $post_meta['meta-prayer-name'][0];
 		$prayer_email = $post_meta['meta-prayer-email'][0];
+		$prayer_location = $post_meta['meta-prayer-location'][0];
+		$prayer_country = $post_meta['meta-prayer-location-country-long'][0];
+
 		?>
-			<div class="echo-admin echo-submitter">
+			<div class="echo-admin echo-location">
 				<div class="avatar">
 					<?php echo get_avatar( $prayer_email, 26 ); ?>
 				</div>
 				<div class="details">
-					<?php echo $prayer_name; ?>
+					<?php echo $prayer_name; ?><br />
+					<?php if ( ! empty($lat) && ! empty($long) ): ?>
+					<?php echo $prayer_location . " (" . $prayer_country . ")"; ?><br />
+					Coord: <?php echo '<a href="http://maps.google.com/?ie=UTF8&hq=&ll=' . $lat . ',' . $long . '&z=13" target="_blank">' . $lat . ' / ' . $long . '</a>'; ?>
+					<?php endif; ?>
 				</div>
 			<div>
 		<?php
+
 	}
-	if ($column_name == 'prayer_coordinates') {
-
-		if ( ! empty($lat) && ! empty($long) ) {
-			echo $lat . "<br />" . $long . "<br />";
+	if ($column_name == 'prayer_submitter' ) {
+		
+	}
+	if ($column_name == 'prayer_answered') {
+		$answered = $post_meta['meta-prayer-answered'][0];
+		
+		if ($answered) {
+			echo "Yes";
 		}
-
+		else {
+			echo "No";
+		}
+	}
+	if ($column_name == 'prayer_anonymous') {
+		$anonymous = $post_meta['meta-prayer-anonymous'][0];
+		
+		if ($anonymous) {
+			echo "Yes";
+		}
+		else {
+			echo "No";
+		}
 	}
 }
 add_filter( 'manage_prayer_posts_columns', 'echo_prayers_columns_head' );
