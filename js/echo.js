1c9cd445c9d30747e43ac35132dd3a6c6a303fcc
Kaleb Heitzman <kalebheitzman@gmail.com>
2015-12-28 21:05:10 -0500

    added click detect on prayer button

--- js/echo.js
+++ js/echo.js
@@ -1,4 +1,11 @@
 (function($) {
 
+	// record prayed for request
+	$('span[data-prayer-click]').click(function() {
+		var post_id = $(this).attr('data-prayer-click');
+		console.log(post_id);
+
+		return false;
+	});
 	
 })(jQuery);
