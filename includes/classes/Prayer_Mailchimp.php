5d41cba21be7739a96b90c6c2452932219462fdf
Kaleb Heitzman <kalebheitzman@gmail.com>
2016-01-05 14:42:20 -0500

    added mailchimp skeleton

---
+++ includes/classes/Prayer_Mailchimp.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Mailchimp Class
+ *
+ * Allows frontened users to login via thier email and a token link
+ * 
+ * @package   Prayer
+ * @author 	  Kaleb Heitzman <kalebheitzman@gmail.com>
+ * @link      https://github.com/kalebheitzman/prayer
+ * @copyright 2015 Kaleb Heitzman
+ * @license   GPL-3.0
+ * @version   0.9.0
+ */
+class Prayer_Mailchimp
+{
+	/**
+	 * Class Construct
+	 *
+	 * @since  0.9.0
+	 */
+	public function __construct()
+	{
+
+	}
+
+	/**
+	 * Authorize Mailchimp
+	 *
+	 * @since  0.9.0
+	 */
+	public function authorize_mailchimp()
+	{
+
+	}
+
+	/**
+	 * Sync List
+	 *
+	 * @since 0.9.0
+	 */
+	public function sync_mailchimp_list()
+	{
+
+	}
+	
+	/**
+	 * Sync Segment
+	 *
+	 * @since  0.9.0
+	 */
+	public function sync_mailchimp_segment()
+	{
+
+	}
+	
+}
