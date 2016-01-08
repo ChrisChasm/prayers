ad0a2ed88758ae6cbf9ddb193cc21fdc9b02cef1
Kaleb Heitzman <kalebheitzman@gmail.com>
2016-01-07 19:22:11 -0500

    added skeleton code to form

--- templates/content-prayers-mailchimp-signup.php
+++ templates/content-prayers-mailchimp-signup.php
@@ -1,1 +1,36 @@
-Signup form
+<p>Subscribed to our community prayer updates.</p>
+
+<form action="" method="post" id="prayer-mailchimp-form">
+    
+	<p class="indicates-required"><span class="asterisk">*</span> indicates required</p>
+
+	<p>
+		<label for="email">Email Address  <span class="asterisk">*</span></label>
+		<input type="email" value="" name="email" class="required email">
+	</p>
+
+	<p>
+		<label for="fname">First Name </label>
+		<input type="text" value="" name="fname" class="">
+	</p>
+
+	<p>
+		<label for="lname">Last Name </label>
+		<input type="text" value="" name="lname" class="">
+	</p>
+
+    <strong>Prayer Interests </strong>
+    <ul>
+    	<li><input type="checkbox" value="" name="" id=""><label for="">Health</label></li>
+		<li><input type="checkbox" value="" name="" id=""><label for="">Missions</label></li>
+		<li><input type="checkbox" value="" name="" id=""><label for="">Needs</label></li>
+		<li><input type="checkbox" value="" name="" id=""><label for="">Supplies</label></li>
+		<li><input type="checkbox" value="" name="" id=""><label for="">Answered Prayers</label></li>
+		<li><input type="checkbox" value="" name="" id=""><label for="">Travel</label></li>
+	</ul>
+
+	<p>
+		<input type="submit" value="Subscribe" name="subscribe" class="button">
+	</p>
+
+</form>
