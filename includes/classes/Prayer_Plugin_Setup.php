69695144b6d398030ec74827bf16f82d348ece0e
Kaleb Heitzman <kalebheitzman@gmail.com>
2016-01-11 14:32:34 -0500

    wrong cp

--- includes/classes/Prayer_Plugin_Setup.php
+++ includes/classes/Prayer_Plugin_Setup.php
@@ -1,87 +1,148 @@
 <?php
 /**
- * Plugin Helpers
+ * Activate Plugin
  *
- * Repeatable pieces of code to use through out the plugin.
- * 
- * @package   Prayer
- * @author    Kaleb Heitzman <kalebheitzman@gmail.com>
- * @link      https://github.com/kalebheitzman/prayer
- * @copyright 2015 Kaleb Heitzman
- * @license   GPL-3.0
- * @version   0.9.0
+ * Requires WP REST API plugin for JSON feeds
+ * Installs a user to associate front end prayer submissions to. If there is a
+ * cleaner way than using wp_die to require dependencies for plugins then I'll
+ * add it in.
+ *
+ * Future: find a better way to require dependencies other than wp_die.
+ *
+ * @since 0.9.0 
  */
 
-class Prayer_Plugin_Helper
+class Prayer_Plugin_Setup
 {
+
+    protected $plugin_path;
+
+    public function __construct() {
+        $path = realpath( plugin_dir_path(__FILE__) . "../../plugin.php" );
+        $this->plugin_path = $path;
+
+        register_activation_hook( $this->plugin_path, array( 'Prayer_Plugin_Setup', 'plugin_activate' ) );
+        register_deactivation_hook( $this->plugin_path, array( 'Prayer_Plugin_Setup', 'plugin_deactivate' ) );
+        register_uninstall_hook( $this->plugin_path, array( 'Prayer_Plugin_Setup', 'plugin_uninstall' ) );
+    }
+   
     /**
-     * Parse Location
+     * Activate Hook
      *
-     * Takes a location string, such as Raleigh, NC and passes it to the Google
-     * geocode API. The API returns a JSON encoded response where location data
-     * like latitude, longitude, country codes, etc are extracted.
-     * 
-     * @param  string User inputted location
-     * @return array Parsed location data
      * @since 0.9.0 
      */
-    static public function parse_location( $location = null ) {
-        if ( is_null($location) ) return;
-        
-        // prep the address
-        $prepAddr = str_replace(' ', '+', $location);
-        // send the address to google
-        $geocode=file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
-        // decode the output
-        $output= json_decode($geocode);
-        // build a parsed location array to return
-        $parsed_location['formatted_address'] = $output->results[0]->formatted_address; // Lexington, KY, USA
-        $parsed_location['lat'] = $output->results[0]->geometry->location->lat;
-        $parsed_location['long'] = $output->results[0]->geometry->location->lng;
-        $parsed_location['country_long'] = $output->results[0]->address_components[3]->long_name;
-        $parsed_location['country_short'] = $output->results[0]->address_components[3]->short_name;
+    public function plugin_activate(){
 
-        return $parsed_location;
+        // Require parent plugin
+        if ( ! is_plugin_active( 'rest-api/plugin.php' ) and current_user_can( 'activate_plugins' ) ) {
+            // Stop activation redirect and show error
+            wp_die('Sorry, but this plugin requires the <a href="https://wordpress.org/plugins/rest-api/">WP REST API (Version 2)</a> to be installed and active. <br><a href="' . admin_url( 'plugins.php' ) . '">&laquo; Return to Plugins</a>');
+        }
+
+        // install default options
+        if ( ! get_option('prayer_settings_options') ) {
+            $options = array(
+                'notification_user' => 1,
+                'prayer_form_response' => __( 'We have received your prayer request.', 'prayer' ),
+                'button_primary_color' => '#2582EA',
+                'button_secondary_color' => '#45D680',
+                'button_text_color' => '#ffffff',
+                'taxonomy_background_color' => '#efefef',
+                'taxonomy_text_color' => '#333333',
+                'categories_enabled' => "1",
+                'tags_enabled' => "1",
+            );
+            add_option( 'prayer_settings_options', $options );
+        }
+
+
+        // create the default prayer user and set permissions to contributer.
+        $username = 'prayer';
+        if( null == username_exists( $username ) ) {
+
+            $password = wp_generate_password( 12, true );
+            $user_id = wp_create_user( $username, $password );
+            
+            $userdata = array(
+                    'ID' => $user_id,
+                    'nickname' => 'Prayer',
+                    'display_name' => 'Prayer',
+                    'first_name' => 'Prayer',
+                    'description' => 'User for Prayer Plugin submission.',
+                    'role' => 'contributer'
+                );
+            wp_update_user( $userdata );
+
+        }
+
+        // create tables to store mailchimp integration info
+        global $wpdb;
+        $table_name = $wpdb->prefix . "prayers_mailchimp";
+        $charset_collate = $wpdb->get_charset_collate();
+
+        $sql = "CREATE TABLE $table_name (
+            id mediumint(11) NOT NULL AUTO_INCREMENT,
+            post_id mediumint(11) NOT NULL,
+            prayed_for smallint(1),
+            email_sent smallint(1),
+            PRIMARY KEY id (id),
+            UNIQUE KEY post_id (post_id)
+        ) $charset_collate;";
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        $db = dbDelta( $sql );
+
     }
 
     /**
-     * Save Location Data to Meta
+     * Deactivate Hook
      *
-     * Saves location to different meta fields. I may serialize this into one meta
-     * field for the future to save on db calls.
-     * 
-     * @param integer ID
-     * @param array Parsed location data
+     * @since 0.9.0 
+     */
+    public function plugin_deactivate() {
+
+    }
+
+    /**
+     * Uninstall Hook
+     *
      * @since 0.9.0
      */
-    static public function save_location_meta( $id = 0, $location = null ) {
-        if ( is_null($location) || $id == 0 ) return;
+    public function plugin_uninstall() {
 
-        update_post_meta( $id, 'prayer-location-latitude', $location['lat'] );
-        update_post_meta( $id, 'prayer-location-longitude', $location['long'] );
-        update_post_meta( $id, 'prayer-location-formatted-address', $location['formatted_address'] );
-        update_post_meta( $id, 'prayer-location-country-long', $location['country_long'] );
-        update_post_meta( $id, 'prayer-location-country-short', $location['country_short'] );
-    }
-
-    /**
-     * Recursive Array Search
-     * @param  String $needle  
-     * @param  Array $haystack
-     * @return boolean
-     */
-    static function in_array_rec($needle, $haystack) { 
-       
-        if ( in_array($needle, $haystack) ) { return true; } 
-       
-        foreach($haystack as $elem) 
+        // delete all prayer requests
+        $prayers = get_posts( array(
+                'post_type' => 'prayer',
+            )
+        );
+        foreach ( $prayers as $prayer )
         {
-            if ( is_array($elem) && self::in_array_rec($needle, $elem) )
-            {
-                return true;
-            }
+            wp_delete_post( $prayer->ID, true );
         }
-        return false; 
-    } 
+
+        // delete prayer categories
+        $categories = get_terms( 'prayer_category' );
+        foreach ( $categories as $category )
+        {
+            wp_delete_term( $category->ID, 'prayer_category ');
+        }
+
+        // delete prayer tags
+        $tags = get_terms( 'prayer_tags' );
+        foreach ( $tags as $tag )
+        {
+            wp_delete_term( $tag->ID, 'prayer_tags ');
+        }
+
+
+        // delete prayer settings
+        delete_option( 'prayer_settings_options' );
+        delete_option( 'prayer_mailchimp_list_id' );
+        delete_option( 'prayer_mailchimp_list_name' );
+
+        // delete the prayer user
+        $user = get_user_by( 'login', 'prayer' );
+        wp_delete_user( $user->id );
+
+    }
 
 }
